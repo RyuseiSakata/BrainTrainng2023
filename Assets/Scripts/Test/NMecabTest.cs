62c3565a34fdf550a22f53e48330a84db7913b96
hirokazu1108 <k.1108.hiro@gmail.com>
2023-08-29 23:41:02 +0900

    テスト用追加

--- Assets/Scripts/Test/NMecabTest.cs
+++ Assets/Scripts/Test/NMecabTest.cs
@@ -5,19 +5,21 @@ using UnityEngine.UI;
 
 public class NMecabTest : MonoBehaviour
 {
+    [SerializeField] string checkString;
+    [SerializeField] List<string> collectList = new List<string>();
     [SerializeField] Text debugStateText;
     [SerializeField] Text text;
     void Start()
     {
         debugStateText.text = "State: before Check()";
-        var list = (new Jage()).Check("‚©‚è‚ñ‚²‚¤");
+        var list = (new Jage()).Check(checkString);
         debugStateText.text = "State: after Check()";
 
         debugStateText.text = "State: before forEach";
         list.ForEach(e =>
         {
             text.text = text.text + "\n" + e;
-            Debug.Log("TEST:" + e);
+            collectList.Add(e);
         });
         debugStateText.text = "State: after forEach";
     }
