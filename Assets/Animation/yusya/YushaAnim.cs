315f17fe64d75d01c9e6e54777e6f2df92b5fdb4
Ryusei <87124964+RyuseiSakata@users.noreply.github.com>
2023-09-11 03:05:52 +0900

    アニメーションの追加

---
+++ Assets/Animation/yusya/YushaAnim.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YushaAnim : MonoBehaviour
+{
+    int num;
+    [SerializeField] private GameObject gameObject1;
+    [SerializeField] private GameObject gameObject2;
+    public Script_SpriteStudio6_Root scriptRoot;
+    private Yusha yusha;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
