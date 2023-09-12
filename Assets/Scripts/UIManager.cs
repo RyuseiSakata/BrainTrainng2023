8a68dcc8dc2f16cac7629a45bdb99c3f5e268be8
hirokazu1108 <k.1108.hiro@gmail.com>
2023-09-12 14:30:08 +0900

    スコアを6桁で表示

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -57,7 +57,7 @@ public class UIManager : MonoBehaviour
                 break;
             case TextKinds.Score:
                 if(scoreText!=null)
-                    scoreText.text = "スコア："+value.ToString("0000");
+                    scoreText.text = "スコア："+value.ToString("000000");
                 break;
         }
     }
